12011358c56f6e43ff67015adb6ffea1736ab587
giovannimhern <giovannimhern@uchicago.edu>
2024-02-28 18:59:28 -0600

    Initial Commit

---
+++ socially/sociallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sociallyApp.swift
+//  socially
+//
+//  Created by Giovanni Maya on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct sociallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
